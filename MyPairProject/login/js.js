191e3c10e475dbe46487fe533133b2eab4c1d688
Nacef19 <117386346+Nacef19@users.noreply.github.com>
2022-12-09 02:27:15 +0100

    Update

--- MyPairProject/login/js.js
+++ MyPairProject/login/js.js
@@ -1,9 +1,9 @@
 function validate(){
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-   if(username == "med17nacef" && password == "azerty13"){
+   if((username == "med13nacef" && password == "azerty13") || (username == "med17ali" && password == "azerty17")){
     alert("Login successfully to watch TN"); 
-     window.location = "../html/home.html";
+     window.location = "../home/html.html";
    }
      else {
       alert("You have enetered wrong inputs")
